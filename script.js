319426828dd28c7704cbe25cba4e6c3b6cbe49ec
Deep Dave <deepdave2534@gmail.com>
2025-03-08 18:30:36 +0530

    Edit script.js

--- script.js
+++ script.js
@@ -2,19 +2,26 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1;
 }
 
-console.log(getComputerChoice())
+let compChoice = 0
+
+compChoice = getComputerChoice();
+
+console.log(compChoice)
 
 function getHumanChoice() {
     let choice = prompt("Enter rock, paper, or scissors:").toLowerCase();
     return choice;
 }
 
-console.log("You chose:", getHumanChoice());
+let humChoice = getHumanChoice();
+
+console.log("You chose:", humChoice);
 
 // 1 => rock  2 => paper  3 => scissors
 
 let humanScore = 0
 let computerScore = 0
+
 
 function playRound(humanChoice, computerChoice) {
     if(humanChoice == computerChoice) {
@@ -23,8 +30,15 @@ function playRound(humanChoice, computerChoice) {
     else if((humanChoice == 1 && computerChoice == 2) || (humanChoice == 2 && computerChoice == 3) || 
             (humanChoice == 3 && computerChoice == 1)) {
         console.log("Computer wins!!");
+        computerScore++;
     }
     else {
         console.log("Human wins!!");
+        humanScore++;
     }
 }
+
+playRound(humChoice, compChoice);
+
+console.log(humanScore);
+console.log(computerScore);
